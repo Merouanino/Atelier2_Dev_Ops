bcd626f4e6ebd381f4ae5338d6f49c354d568306
AIT MOKHTAR Ahmed Merouane <aitmokhtar@et.intechinfo.fr>
2026-09-18 15:23:03 +0200

    ajout compose

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 ﻿from flask import Flask, jsonify
+import redis
+import os
 
 app = Flask(__name__)
 
 ALERT_THRESHOLD = 25
+
+
+def get_redis_client():
+    return redis.Redis(host=os.getenv("REDIS_HOST", "redis"), port=6379)
 
 
 def alert_threshold():
@@ -23,6 +29,13 @@ def health():
 @app.route("/status")
 def status():
     return jsonify(service="projet-devops-groupe-demo", version="1.0"), 200
+
+
+@app.route("/visits")
+def visits():
+    r = get_redis_client()
+    count = r.incr("visits")
+    return jsonify(visits=count), 200
 
 
 if __name__ == "__main__":
